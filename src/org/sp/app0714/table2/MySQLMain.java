07be36a28aac0b8279bb87a724a07dd483dbc391
songpa <lecture230502@gmail.com>
2023-07-14 12:48:24 +0900

    4교시

--- src/org/sp/app0714/table2/MySQLMain.java
+++ src/org/sp/app0714/table2/MySQLMain.java
@@ -4,6 +4,8 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.swing.JFrame;
@@ -15,7 +17,7 @@ public class MySQLMain extends JFrame{
 	JScrollPane scroll;
 	String[][] data= {};
 	String[] column= {"member_idx","id","name","phone"};
-	String url="jdbc:mysql://localhost:3306/javase";
+	String url="jdbc:mysql://localhost:3306/javase?characterEncoding=utf8";
 	String user="root";
 	String pass="1234";
 	Connection con;
@@ -43,6 +45,7 @@ public class MySQLMain extends JFrame{
 		});
 		
 		connect();
+		getList();
 	}
 	
 	public void connect() {
@@ -59,6 +62,45 @@ public class MySQLMain extends JFrame{
 			}			
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void getList() {
+		PreparedStatement pstmt=null; //쿼리문 수행 객체 
+		ResultSet rs=null; //표를 표현한 객체
+		
+		try {
+			pstmt=con.prepareStatement("select * from member");
+			rs=pstmt.executeQuery();
+			
+			rs.last();
+			int row=rs.getRow();
+			System.out.println(row);
+			
+			//rs를 이차원배열로 변환한 후, JTable에 적용하자
+			//이차원배열 준비해놓기 
+			String[][] record=new String[row][4];
+			
+			//제일 마지막으로 보냈던 커서를 원상복귀 시키자 
+			rs.beforeFirst(); 
+			rs.next();
+			
+			int member_idx=rs.getInt("member_idx"); //[][0]
+			String id=rs.getString("id");//[][1]
+			String name=rs.getString("name");//[][2]
+			String phone=rs.getString("phone");//[][3]
+			
+			//하나의 레코드를 이차원배열의 한 일차원 	배열로 옮기자 
+			record[0][0]=Integer.toString(member_idx);
+			record[0][1]=id;
+			record[0][2]=name;
+			record[0][3]=phone;
+			
+			table = new JTable(record, column);
+			//this.updateUI();
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
